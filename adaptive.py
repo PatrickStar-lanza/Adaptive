97a58c782adea098e2dfcc19fed3b08791abdd40
Zheng Liu <122706384+PatrickStar-lanza@users.noreply.github.com>
2023-04-12 16:29:01 +0100

    Update adaptive.py

--- adaptive.py
+++ adaptive.py
@@ -247,7 +247,7 @@ class Decoder( nn.Module ):
             h_t, states = self.LSTM( x_t, states )
             
             # Save hidden and cell
-            hiddens[ :, time_step, : ] = h_t  # Batch_first
+            hiddens[ :, time_step, : ] = h_t.squeeze(1)  # Batch_first
             cells[ time_step, :, : ] = states[ 1 ]
         
         # cell: Batch x seq_len x hidden_size
